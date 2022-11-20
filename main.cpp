1b4b1a966955e2fd414d0809cab4443d00aabbb7
Andrea-Bighelli <117951296+Andrea-Bighelli@users.noreply.github.com>
2022-11-20 11:10:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0, b=0, area=0;
+    int opzione = 0;
+
+    cin >> a >> b >> opzione;
+
+    switch (opzione) {
+        case 0:
+            area = a*b/2;
+            cout << area;
+            break;
+        case 1 :
+            area = a*a;
+            cout << area;
+            break;
+        case 2:
+            area = a*b;
+            cout << area;
+            break;
+        default:
+            cout << "opzione non valida" << endl;
+    }
+    return 0;
 }
+
+
